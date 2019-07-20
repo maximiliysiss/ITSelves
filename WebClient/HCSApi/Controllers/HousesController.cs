32fcb3c7f11effac6fd63ec8dcae184eb32370f8
Maxim Zimin <zim.ma2010@yandex.ru>
2019-07-20 18:30:28 +0300

    Init HCS

---
+++ WebClient/HCSApi/Controllers/HousesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HCSApi;
+using HCSApi.Models;
+
+namespace HCSApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HousesController : ControllerBase
+    {
+        private readonly DatabaseHCSAPI _context;
+
+        public HousesController(DatabaseHCSAPI context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Houses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<House>>> GetHouse()
+        {
+            return await _context.House.ToListAsync();
+        }
+
+        // GET: api/Houses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<House>> GetHouse(int id)
+        {
+            var house = await _context.House.FindAsync(id);
+
+            if (house == null)
+            {
+                return NotFound();
+            }
+
+            return house;
+        }
+
+        // PUT: api/Houses/5
+        [HttpPut]
+        [Route("edit")]
+        public async Task<IActionResult> Edit(House house)
+        {
+            _context.Entry(house).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Houses
+        [HttpPost]
+        [Route("add")]
+        public async Task<ActionResult<House>> Add(House house)
+        {
+            _context.House.Add(house);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHouse", new { id = house.ID }, house);
+        }
+
+        // DELETE: api/Houses/5
+        [HttpDelete("{id}")]
+        [Route("delete")]
+        public async Task<ActionResult<House>> DeleteHouse(int id)
+        {
+            var house = await _context.House.FindAsync(id);
+            if (house == null)
+            {
+                return NotFound();
+            }
+
+            _context.House.Remove(house);
+            await _context.SaveChangesAsync();
+
+            return house;
+        }
+    }
+}
---
+++ WebClient/HCSApi/Migrations/20190720135200_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HCSApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "House",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Street = table.Column<string>(nullable: true),
+                    HouseNumber = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Area = table.Column<string>(nullable: true),
+                    Index = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_House", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "House");
+        }
+    }
+}
