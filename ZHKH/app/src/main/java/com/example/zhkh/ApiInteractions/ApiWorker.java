54eb77a96106396dc1c41e953989f09b4b5dbb2c
Kubiken <kubiken@yandexx.ru>
2019-07-20 23:01:54 +0300

    Getting token at logscreen

--- ZHKH/app/src/main/java/com/example/zhkh/ApiInteractions/ApiWorker.java
+++ ZHKH/app/src/main/java/com/example/zhkh/ApiInteractions/ApiWorker.java
@@ -1,9 +1,16 @@
 package com.example.zhkh.ApiInteractions;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import com.example.zhkh.ApiInteractions.pojoes.Key;
 import com.example.zhkh.ApiInteractions.pojoes.Token;
+import com.example.zhkh.MainActivity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +23,11 @@ public class ApiWorker
     private Retrofit rf;
     private Gson gson;
     private String baseUrl;
+    private IAuthApi log;
+
+    public IAuthApi getLog() {
+        return log;
+    }
 
     public ApiWorker(String baseUrl) //-"https://your.api.url/v2/"
     {
@@ -29,33 +41,8 @@ public class ApiWorker
             .baseUrl(this.baseUrl)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build();
+
+        log = rf.create(IAuthApi.class);
     }
 
-    public String tryLogin(String token)
-    {
-        final Token[] user = new Token[1];
-        Key key = new Key();
-        key.setKey(token);
-        String res = "000";
-        IAuthApi log = rf.create(IAuthApi.class);
-        log.getAuth(key).enqueue(new Callback <Token>()
-        {
-
-        @Override
-        public void onResponse(Call<Token> call, Response<Token> response)
-        {
-            user[0] =response.body();
-        }
-
-        @Override
-        public void onFailure(Call<Token> call, Throwable t)
-        {
-            user[0] = new Token();
-        }
-
-    });
-
-        res = user[0].getToken();
-        return res;
-    }
 }
--- ZHKH/app/src/main/java/com/example/zhkh/LogScreenActivity.java
+++ ZHKH/app/src/main/java/com/example/zhkh/LogScreenActivity.java
@@ -2,14 +2,26 @@ package com.example.zhkh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zhkh.ApiInteractions.ApiWorker;
+import com.example.zhkh.ApiInteractions.IAuthApi;
+import com.example.zhkh.ApiInteractions.pojoes.Key;
+import com.example.zhkh.ApiInteractions.pojoes.Token;
+
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LogScreenActivity extends AppCompatActivity {
 
     private TextView passField;
+    private String tok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,9 +31,35 @@ public class LogScreenActivity extends AppCompatActivity {
         passField = (TextView)findViewById(R.id.passwordField);
     }
 
-    public void onPasswordButClick(View view)
-    {
+    public void onPasswordButClick(View view) throws InterruptedException {
         ApiWorker aw = new ApiWorker("http://85.143.10.92:8001/");
-        String user = aw.tryLogin("3a88-e9df-bfd8-26f3");
+        IAuthApi login = aw.getLog();
+
+        Key key = new Key();
+        key.setKey("3a88-e9df-bfd8-26f3");
+
+        login.getAuth(key).enqueue(new Callback<Token>()
+        {
+
+            @Override
+            public void onResponse(Call<Token> call, Response<Token> response)
+            {
+                if(!response.isSuccessful())
+                {
+                    System.out.println("We got some troubles. But server is okay");
+                    return;
+                }
+                tok = response.body().getToken();
+                System.out.println("Success! "+tok);
+            }
+
+            @Override
+            public void onFailure(Call<Token> call, Throwable t)
+            {
+                System.out.println(t.getMessage());
+            }
+
+        });
     }
+
 }
