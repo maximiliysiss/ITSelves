c4eff953b3ec61fef3c10476cdb10d95fb7827b2
Kubiken <kubiken@yandexx.ru>
2019-07-20 15:26:32 +0300

    Added Retrofit lib+gson converter

--- ZHKH/app/src/main/java/com/example/zhkh/MainActivity.java
+++ ZHKH/app/src/main/java/com/example/zhkh/MainActivity.java
@@ -10,8 +10,23 @@ import android.widget.TextView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+
+//    Gson gson = new GsonBuilder()
+//            .setLenient()
+//            .create();
+//
+//    Retrofit retrofit =  new Retrofit.Builder()
+//            .baseUrl("https://your.api.url/v2/")
+//            .addConverterFactory(GsonConverterFactory.create(gson))
+//            .build();
 
     NavController navController;
 
